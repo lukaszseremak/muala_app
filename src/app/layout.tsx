57eaa5fbe916e1146518ee4aef04bf7aeac02897
Seremak Lukasz <lukasz.seremak@nokia.com>
2024-02-04 13:50:10 +0100

    feat: inject NavBar in layout.tsx instead of every single page separatly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import NavMenu from "@/components/ui/nav-menu";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Muala App',
-  description: 'Szukaj lokali Muala w Twojej okolicy',
-}
+  title: "Muala App",
+  description: "Szukaj lokali Muala w Twojej okolicy",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pl">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavMenu />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
