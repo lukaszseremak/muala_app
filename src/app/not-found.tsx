005734bde3455b73fbb41c21bbbfea429cc5a6ae
Seremak Lukasz <lukasz.seremak@nokia.com>
2024-02-12 09:09:34 +0100

    feat: create default not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full align-middle center-object text-muala text-center">
+      <h1 className="text-2xl">404 - Page Not Found</h1>
+    </div>
+  );
+}
